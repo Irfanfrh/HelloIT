c7e784f675529469cfb582fd1a03e97a53b867a6
Irfanfrh <hakimirfan837@gmail.com>
2021-04-30 22:56:49 +0800

    first commit

---
+++ HelloIT.java
@@ -0,0 +1,12 @@
+public class HelloIT {
+   public static void main(String[] args) {
+       
+        //menampilkan string
+       System.out.println("Hello");
+       System.out.println("Informatika");
+    }
+       
+    }
+      
+
+    
